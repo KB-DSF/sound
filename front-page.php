d7c2eb8c659ea9c5537a987c5fe7a010384b91ff
Karianne <kb@kbs-MacBook-Pro.local>
2014-08-01 22:01:59 -0700

    made a 'front page' specific template
    that's all!

--- front-page.php
+++ front-page.php
@@ -14,6 +14,10 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div id="test1" class="tester" data-stellar-background-ratio="0.5"><span>TEST 1</span></div>
+			<div id="test2" class="tester" data-stellar-background-ratio="0.5"><span>TEST 2</span></div>
+			<div id="test3" class="tester" data-stellar-background-ratio="0.5"><span>TEST 3</span></div>
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>

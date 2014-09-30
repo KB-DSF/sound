1ca197e701b2a048f36e29f0ec60174b5d50319c
Karianne - office comp <dsf@Davids-iMac.local>
2014-09-30 16:46:13 -0700

    Refined front page mockup
    Added image and buttons at bottom, mostly

--- front-page.php
+++ front-page.php
@@ -19,21 +19,12 @@ get_header(); ?>
 			<div id="test2" class="tester" data-stellar-background-ratio="0.5"><span class="tester-title">Tourism and the new Howe Sound vision</span><br /><div class="tester-deck">Howe Sound is a unique ecosystem, home to a diversity of terrestrial and aquatic species and habitats. It is also home to many coastal communities...</div></div>
 			<div id="break2" class="break"><span>... people like you take a stand.</span></div>
 			<div id="test3" class="tester" data-stellar-background-ratio="0.5"><span class="tester-title">Diving below the surface</span><br /><span class="tester-deck">Howe Sound is a unique ecosystem, home to a diversity of terrestrial and aquatic species and habitats. It is also home to many coastal communities...</span></div>
-						<div id="break2" class="break"><span><a href="">take action now</a></span></div>
+						<div id="action" class="break"><span><a href="">TAKE ACTION NOW</a></span>
+						<a href=""><div id="volunteer">Volunteer</div></a>
+						<a href=""><div id="signup">Add your voice</div></a>
+						</div>
 
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					/*if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;*/
-				?>
-
-			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
